55c4dd868f049a5166d9512c9c476ed2573e020f
Paweł Wójcik <pawel.wojcik5@gmail.com>
2024-02-08 13:46:57 -0800

    Move printing functions to a separate file

--- soc.py
+++ soc.py
@@ -9,7 +9,8 @@ molecule.
 import argparse
 import sys
 import numpy as np
-import cmath as m
+
+import printing as pr
 
 eV2cm = 8065.479
 cm2eV = 1.0/eV2cm
@@ -32,195 +33,6 @@ def get_args():
                         type=float, help="Print threshold")
     args = parser.parse_args()
     return args
-
-
-def state2latex(state):
-    irrep = state['irrep']
-    irrepN, irrep_lbl = irrep.split('/')
-
-    irrep_lttr = irrep_lbl[0]
-    irrep_decorator = ""
-    irrep_extra = irrep_lbl[1:]
-    if irrep_extra == "":
-        irrep_decorator = ""
-    elif irrep_extra == "'":
-        irrep_decorator = r' ^{\prime}'
-    elif irrep_extra == '"':
-        irrep_decorator = r' ^{\prime\prime}'
-    else:
-        irrep_decorator = r' _{' + irrep_extra + '}'
-
-    multiplicity = state['multiplicity']
-    spin_z = state['spin_z']
-    out = r'\ket{' + irrepN + ' ^{' + str(multiplicity) + '} '
-    out += irrep_lttr + irrep_decorator + ', ' + f"{spin_z:2d}" + '}'
-    return out
-
-
-def print_vector_latex(v,
-                       already_sorted: bool = False,
-                       polar: bool = False,
-                       norm_threshold: float = 0.1,
-                       latex_eom_states: bool = False):
-    r"""
-    Prints an eigenvector of the soc-hamiltonian by listing the basis
-    vectors that have an amplitude (modulus square of its coefficient)
-    larger than 0.01.
-    Output is formatted in a latex format
-
-    Parameters
-    ----------
-    v : eigenvector
-        v = c_{i_0} |i_0> + \ldots
-        In each pair the [0] element is i_0, and the [1] element is c_{i_0}
-    polar (bool, optional): $1 ^2 \Sigma$  vs '1'. Defaults to False.
-    norm_threshold (float, optional):
-        print "c_{i_0} |i_0>" if |c_{i_0}| > norm_threshold
-    latex_eom_states (bool, optional): $1 ^2 \Sigma$  vs '1'.
-        Defaults to False.
-
-
-    Returns
-    -------
-    output : string
-        Latex-formatted presentation of v
-
-    """
-    vector = [[i, val] for i, val in enumerate(v)]
-    vector.sort(key=lambda z: abs(z[1]), reverse=True)
-    small_contributions = False
-    output = ""
-    output += "$"
-    for coeff in vector:
-        norm = abs(coeff[1])
-        if norm > norm_threshold:
-            if polar:
-                phi = m.phase(coeff[1]) / m.pi
-                # Positive number on the real axis
-                if abs(phi) < 0.01:
-                    output += f"+{norm:4.3f}"
-                # Negative number on the real axis
-                elif abs(phi) > 0.95 and abs(phi) < 1.05:
-                    output += f"-{norm:4.3f}"
-                # On the imaginary axis up
-                elif phi > 0.45 and phi < 0.55:
-                    output += f"+{norm:4.3f}i"
-                # On the imaginary axis down
-                elif phi < -0.45 and phi > -0.55:
-                    output += f"-{norm:4.3f}i"
-                # The number does not lie on any axis – need to show the phase
-                else:
-                    output += f"{norm:+4.3f}"
-                    output += r"e^{" + f"{phi:+3.2f}" + r"\pi i} "
-            else:
-                output += f"{coeff[1].real:+3.2f}"
-                output += f"{coeff[1].imag:+3.2f}i"
-            # HINT: `label` is the right place to go fancy
-            label = coeff[0]
-            output += r"\ket{"
-            if latex_eom_states:
-                output += f"{label}"
-            else:
-                output += f"{coeff[0]:2d}"
-            output += "}"
-            output += " "
-        elif norm**2 > 5e-4:
-            small_contributions = True
-    if small_contributions:
-        output += r"+ \ldots"
-    output += "$"
-
-    # trim the leading + sign
-    if output[1] == "+":
-        output = "$" + output[2:]
-
-    return output
-
-
-def print_submatrix(h, rows, cols, title: str = ""):
-
-    rows.sort()
-
-    print(f"Printing: {title}")
-    print('   ', end='')
-
-    if len(h) < rows[-1] + 1:
-        print("Error, subrows of the matrix out of range.", file=sys.stderr)
-        return
-    if len(h[0]) < cols[-1] + 1:
-        print("Error, subcolumns of the matrix out of range.", file=sys.stderr)
-        return
-
-    for col in cols:
-        print(f"       |{col:2d}>  ", end='')
-    print('\n', end='')
-
-    flt_fmt = '+1.2f'
-    empty = ' ' * 5
-    for row in rows:
-        print(f"<{row:2d}|", end='')
-        for x in h[row][cols[0]:cols[-1]+1]:
-            if abs(x.real) < 0.001:
-                real = empty
-            else:
-                real = f'{x.real:{flt_fmt}}'
-
-            if abs(x.imag) < 0.001:
-                imag = empty
-            else:
-                imag = f'{x.imag:{flt_fmt}}'
-
-            print(f"({real},{imag})", end='')
-        print("\n", end='')
-
-
-def print_matrix(h, title: str = ""):
-    print(f"Printing : {title}")
-    print('   ', end='')
-    for i, row in enumerate(h):
-        print(f"       |{i:2d}>  ", end='')
-    print('\n', end='')
-
-    flt_fmt = '+1.2f'
-    empty = ' ' * 5
-    for i, row in enumerate(h):
-        print(f"<{i:2d}|", end='')
-        for x in row:
-            if abs(x.real) < 0.001:
-                real = empty
-            else:
-                real = f'{x.real:{flt_fmt}}'
-
-            if abs(x.imag) < 0.001:
-                imag = empty
-            else:
-                imag = f'{x.imag:{flt_fmt}}'
-
-            print(f"({real},{imag})", end='')
-        print("\n", end='')
-
-
-def print_states_positions(states):
-    state_id2name = {}
-    for state in states:
-        irrep = state['irrep']
-        multiplicity = state['multiplicity']
-        pos = state['position']
-        first = pos[0]
-        last = pos[1]
-        for n, p in enumerate(range(first, last)):
-            # s > 0 by definition
-            # spin_z goes from -s up to s
-            # multiplicity is 2s+1
-            # s = 0.5 * (multiplicity - 1)
-            spin_z = - (multiplicity - 1) // 2 + n
-            print(f"|{p:2d}> = |{irrep}, {multiplicity}, {spin_z}>")
-            state_id2name[p] = {
-                "irrep": irrep,
-                "multiplicity": multiplicity,
-                "spin_z": spin_z,
-            }
-    return state_id2name
 
 
 def introduce_order(states):
@@ -365,63 +177,6 @@ def prepare_subblock_ranges(args, dim):
     return rows, cols
 
 
-LATEX_TABLE_HEADER = r"""\begin{tabular}{|c|r|r|l|}
-\hline
-id & $E _{ex}$, cm$^{-1}$ & $E _{ex}$, eV & eigenvector \\
-\hline"""
-
-LATEX_TABLE_TAIL = r"""\hline
-\end{tabular}"""
-
-
-LATEX_TABLE_HEADER_EXTRA = r"""
-\begin{tabular}{|l|l|}
-\hline
-nickname & state \\
-\hline"""
-
-
-def deal_with_spectrum_printing(args, cols, evalues, evectors, state_id2name):
-
-    if args.eigenvectors == "":
-        return
-
-    if 'l' in args.eigenvectors:
-        print(LATEX_TABLE_HEADER_EXTRA)
-        for col in cols:
-            line = r'$\ket{' + f"{col:2d}" + r'}$'
-            line += " & "
-            line += "$"
-            line += state2latex(state_id2name[col])
-            line += "$"
-            line += r' \\'
-            print(line)
-        print(LATEX_TABLE_TAIL)
-
-    print("\n")
-    if "l" in args.eigenvectors:
-        print(LATEX_TABLE_HEADER)
-
-    for id in cols:
-        energy = evalues[id]
-        vector = evectors[:, id]
-        print_threshold = args.threshold
-        vector_str = print_vector_latex(
-            vector, polar=True, norm_threshold=print_threshold)
-        if "a" in args.eigenvectors:
-            print(f"{id:2d}: {energy:9.2f} cm-1"
-                  f" = {energy*cm2eV:6.3f} eV"
-                  f"\t\t{vector_str}")
-        elif "l" in args.eigenvectors:
-            print(f"{id:2d} & "
-                  f"{energy:9.2f} & "
-                  f"{energy*cm2eV:6.3f} & "
-                  f"{vector_str} \\\\")
-
-    if "l" in args.eigenvectors:
-        print(LATEX_TABLE_TAIL)
-
-
 def main():
     args = get_args()
     # The data file is a python script that contains
@@ -448,17 +203,15 @@ def main():
 
     hamiltonian = construct_Hamiltonian_matrix(dim, states, socs_aggregate)
 
-    state_id2name = print_states_positions(states)
-
     rows, cols = prepare_subblock_ranges(args, dim)
 
     if args.Hamiltonian is True:
-        print_submatrix(hamiltonian, rows, cols,
-                        title="SOCs + diagonal energies")
+        pr.print_submatrix(hamiltonian, rows, cols,
+                           title="SOCs + diagonal energies")
 
     evalues, evectors = np.linalg.eigh(hamiltonian)
 
-    deal_with_spectrum_printing(args, cols, evalues, evectors, state_id2name)
+    pr.deal_with_spectrum_printing(args, cols, evalues, evectors, states)
 
     if args.show_largest is True:
         find_largest_soc(socs_aggregate)
